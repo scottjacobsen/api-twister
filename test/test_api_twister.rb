c024eaeac9d4cc22cedd269454236f8d740a51e6
Tee Parham <tee@stackpilot.com>
2011-01-19 14:17:11 -0700

    mocha is already required in test_helper

--- test/test_api_twister.rb
+++ test/test_api_twister.rb
@@ -1,5 +1,5 @@
 require File.expand_path('test_helper.rb', File.dirname(__FILE__))
-require 'mocha'
+
 class ApiTwisterTest < Test::Unit::TestCase
 
   context "A class with the default api specification" do
